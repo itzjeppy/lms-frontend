da952970ce2b9e42f75185b4766c99efba735fc2
saeechormunge23 <111693070+saeechormunge23@users.noreply.github.com>
2024-12-02 16:49:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./theme";
-import Layout from "../../lms-frontend/src/Components/Common/Layout.jsx"; // Ensure this path is correct
 import AppRoutes from "./Components/Utils/AppRoutes";
-
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Router>
-        <Layout>
-          <AppRoutes/>
-        </Layout>
-      </Router>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
