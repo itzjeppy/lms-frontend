4b28737b0c33df2008c36e9b8ee685a2b3b27997
jhollinejepson <jhollinej@hexaware.com>
2024-12-02 11:43:12 +0530

    Dashboard Update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { CssBaseline, Box, ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./theme";
-import Navbar from "../../lms-frontend/src/Components/Navbar.jsx";
-import Sidebar from "../../lms-frontend/src/Components/Sidebar.jsx";
+import Layout from "../../lms-frontend/src/Components/Common/Layout.jsx"; // Assuming Layout is stored here
 
 const App = () => {
-  const appBarHeight = 64; // Define a height for the Navbar (default for Material-UI AppBar)
-
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Router>
-        <Navbar />
-        {/* Main Content Wrapper */}
-        <Box sx={{ display: "flex" }}>
-          {/* Sidebar */}
-          <Sidebar appBarHeight={appBarHeight} />
+        <Layout>
           {/* Main Content */}
-          <Box
-            component="main"
-            sx={{
-              flexGrow: 1,
-              p: 3,
-              marginTop: `${appBarHeight}px`, // Offset to account for the Navbar height
-              backgroundColor: theme.palette.background.default,
-            }}
-          >
-            {/* Add your page components here */}
-          </Box>
-        </Box>
+          <Routes>
+            {/* Define your routes and components here */}
+            {/* Example: <Route path="/" element={<HomePage />} /> */}
+          </Routes>
+        </Layout>
       </Router>
     </ThemeProvider>
   );
