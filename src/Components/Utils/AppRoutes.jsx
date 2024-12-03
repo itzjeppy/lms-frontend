2c8485517d8a1aa1b23e0415f7398272c2acb883
saeechormunge23 <111693070+saeechormunge23@users.noreply.github.com>
2024-12-03 23:27:41 +0530

    fixed routes for coupons page

--- src/Components/Utils/AppRoutes.jsx
+++ src/Components/Utils/AppRoutes.jsx
@@ -7,6 +7,8 @@ import LandingPage from "../Misc/LandingPage"
 import SignIn from "../Misc/SignIn";
 import SignUp from "../Misc/SignUp"
 import OffersPage from "../Offers/OffersPage";
+import CouponsPage from "../Coupons/CouponsPage";
+import AddCoupons from "../Coupons/AddCoupon";
 
 const AppRoutes = () => {
   return (
@@ -17,8 +19,9 @@ const AppRoutes = () => {
       <Route path="/dashboard" element={<Layout />}> {/* Layout for dashboard routes */}
         <Route path="offers" element={<OffersPage />} />
         <Route path="add-offer" element={<AddOffers />} /> {/* New route for AddOffers */}
+        <Route path="add-coupon" element={<AddCoupons />} />{" "}
         <Route path="tiers" element={<TiersPage />} />
-        <Route path="coupons" element={<div>Coupons</div>}/>
+        <Route path="coupons" element={<CouponsPage />}/>
         <Route index element={<OffersPage />} /> {/* Default dashboard home */}
       </Route>
     </Routes>
